6c2f3f28509f359a95c33b5dd1e13c53a5c230af
sara1-aidi <sara.aidi@univ-constantine2.dz>
2025-11-11 23:21:04 +0100

    Remove broken lab3_node submodule

--- lab3_node/fileImport.js
+++
@@ -1,8 +0,0 @@
-
-const { mean } = require('./notation');
-
-const notes = [10, 14, 16, 12, 18];
-const avg = mean(notes);
-
-console.log("Notes:", notes);
-console.log("Moyenne:", avg);
--- lab3_node/notation.js
+++
@@ -1,9 +0,0 @@
-
-function mean(arr) {
-  if (!Array.isArray(arr) || arr.length === 0) return 0;
-  const sum = arr.reduce((acc, val) => acc + val, 0);
-  return sum / arr.length;
-}
-
-module.exports = { mean };
-
